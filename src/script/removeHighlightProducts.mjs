e80c977b0066742519ffae1be9cef497aaeb0d2a
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-04-12 19:21:29 +0200

    :sparkles: feat(removeHighlightProducts): add hidden class on mouseout

--- src/script/removeHighlightProducts.mjs
+++ src/script/removeHighlightProducts.mjs
@@ -1,4 +1,6 @@
 export default function highlightProducts(event) {
     const targetedCard = event.currentTarget;
+    const ingredients = targetedCard.querySelector('.ingredients');
+    ingredients.classList.add('hidden');
     targetedCard.classList.remove('highlight_card');
 }
