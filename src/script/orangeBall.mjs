8e3bf819b5d95bc07127684701756dc88f3ace4b
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-04-12 16:53:23 +0200

    :dizzy: animation(balls): update animation on pink and orange ball.

--- src/script/orangeBall.mjs
+++ src/script/orangeBall.mjs
@@ -2,9 +2,9 @@ import gsap from 'gsap';
 
 export default function animationOrangeBall() {
     gsap.to('#orangeCircle', {
-        x: 15,
-        y: 25,
-        duration: 6,
+        x: 25,
+        y: 30,
+        duration: 7,
         repeat: -1,
         yoyo: true,
         ease: 'easeInOutSine',
--- src/script/pinkBall.mjs
+++ src/script/pinkBall.mjs
@@ -3,7 +3,7 @@ import gsap from 'gsap';
 export default function animationPinkBall() {
     gsap.to('#pinkCircle', {
         x: 15,
-        y: 15,
+        y: 30,
         duration: 5,
         repeat: -1,
         yoyo: true,
