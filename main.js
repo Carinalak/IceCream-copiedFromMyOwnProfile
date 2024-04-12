4b1ba9f5250689069bec08add1fc043f90f754eb
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-04-12 15:02:53 +0200

    :sparkles: feat(main.js): import product-json-document and function to render out products

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+import productsData from './products.json';
+import renderProducts from './src/script/renderProducts';
 import './style.scss';
+
+const products = productsData;
+
+renderProducts(products);
